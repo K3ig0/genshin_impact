d5773f17869af10d98617a2ce43b8cb61ad29a14
Tan Zhe Wen <zhewen1186@gmail.com>
2021-01-29 16:40:25 +0800

    Add files via upload

--- resin_time_left.py
+++ resin_time_left.py
@@ -1,24 +1,30 @@
-# -*- coding: utf-8 -*-
-from datetime import datetime, timedelta
-import sys
+#import
+from datetime import datetime as dt, timedelta as td
+#setting
+minPerResin=8
+maxResin=160
+counter=0
+ls={12:[],24:[]}
+#function
+def create_resin_datetime(currentResin, approxResinTimeLeft, resin_goal):
+        if currentResin < resin_goal:
+                required_resin = resin_goal - currentResin
+                minutes_for_required_resin = required_resin * minPerResin - approxResinTimeLeft
+                datetime_for_required_resin = dt.now() + td(minutes=minutes_for_required_resin)
+                formatted_12datetime_for_required_resin = datetime_for_required_resin.strftime("%I:%M%p")
+                formatted_24datetime_for_required_resin = datetime_for_required_resin.strftime("%H:%M")
+                ls[12].append(str(resin_goal)+" resin at "+formatted_12datetime_for_required_resin)
+                ls[24].append(str(resin_goal)+" resin at "+formatted_24datetime_for_required_resin)
+#input
+currentResin=int(input("Current resin: "))
+approxResinTimeLeft=int(input("Approx minute/s left for current resin: "))
+while(counter<=maxResin):
+        counter+=20
+        create_resin_datetime(currentResin, approxResinTimeLeft, counter)
+print("\n[12-hr format] Results:")
+for i in ls[12]: print(i)
+print("\n[24-hr format] Results:")
+for i in ls[24]: print(i)
+input("\nPress Enter to continue...")
 
-MINUTES_PER_RESIN = 8
 
-def process():
-	current_resin = int(input("Enter your current resin: "))
-	print_resin_datetime(current_resin, 20)
-	print_resin_datetime(current_resin, 40)
-	print_resin_datetime(current_resin, 60)
-	print_resin_datetime(current_resin, 160)
-	raw_input("\nPress Enter to continue...")
-
-def print_resin_datetime(current_resin, resin_goal):
-	if current_resin < resin_goal:
-		required_resin = resin_goal - current_resin
-		minutes_for_required_resin = required_resin * MINUTES_PER_RESIN
-		datetime_for_required_resin = datetime.now() + timedelta(minutes=minutes_for_required_resin)
-		formatted_datetime_for_required_resin = datetime_for_required_resin.strftime("%H:%M:%S")
-		print "==> " + str(resin_goal) + " resin at", formatted_datetime_for_required_resin
-
-if __name__ == '__main__':
-    process()
