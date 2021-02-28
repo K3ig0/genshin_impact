210c0ca694b04ff96e52d1364b912a61d20e82fe
Tan Zhe Wen <zhewen1186@gmail.com>
2021-03-01 01:06:46 +0800

    Update resin_time_left.py

--- resin_time_left.py
+++ resin_time_left.py
@@ -3,33 +3,44 @@ from datetime import datetime as dt, timedelta as td
 
 #setting
 minPerResin=8
-ls={12:[],24:[]}
+ls={12:[]}
 thres=[20,40,60,160]
 
 #main
 def process():
-        currentResin=int(input("Current resin: "))
-        approxResinTimeLeft=int(input("Approx minute/s left for current resin: "))
-        for counter in thres: create_resin_datetime(currentResin, approxResinTimeLeft, counter)
-        print("\n[24-hr format] Results:")
-        for i in ls[24]: print(i)
-        bool12=str(input("\nDo you want to view results in 12-hr format too? (y/n): ")).lower()
-        if(bool12 == 'y' or bool12 == 'yes'):
-                print("\n[12-hr format] Results:")
-                for i in ls[12]: print(i)
+        try:
+                hour, minute = input("Fully replenished [hh mm]: ").split(" ")
+                timelist = {"hr":hour, "min":minute}
+                for k, v in timelist.items(): timelist[k] = int(v)
+                if (not hour in range(0,24)) or (not minute in range(0,59)): raise ValueError
+        except ValueError:
+                print("\n***\nPlease try again. Format = hh mm (12 00 for example).\n***\n")
+                process()
+        approxResinTimeLeft = (timelist["hr"]*60)+timelist["min"]
+        create_resin_datetime(approxResinTimeLeft)
+        print("\nResults:")
+        for i in ls[12]: print(i)
         input("\nPress Enter to continue...")
-        
-#function
-def create_resin_datetime(currentResin, approxResinTimeLeft, resin_goal):
-        if currentResin < resin_goal:
-                required_resin = resin_goal - currentResin
-                minutes_for_required_resin = required_resin * minPerResin - approxResinTimeLeft
-                datetime_for_required_resin = dt.now() + td(minutes=minutes_for_required_resin)
-                date=datetime_for_required_resin.strftime("%d/%h/%Y")
-                formatted_12datetime_for_required_resin = datetime_for_required_resin.strftime("%I:%M%p")
-                formatted_24datetime_for_required_resin = datetime_for_required_resin.strftime("%H:%M")
-                ls[12].append("==> "+str(resin_goal)+" resin at "+formatted_12datetime_for_required_resin+", "+date)
-                ls[24].append("==> "+str(resin_goal)+" resin at "+formatted_24datetime_for_required_resin+", "+date)
+
+def intro():
+        print("==> Good day to you, Traveler.")
+        print("==> Please acquire the total time needed to fully replenish your resins.")
+        print("==> Note: You can get it from Map function.\n")
+        print(10*"-"+"\nResin List\n"+10*"-")
+        print("20 resin = 1x Leyline or 1x Domain")
+        print("40 resin = 1x condensed resin or 1x World Boss")
+        print("60 resin = 1x Dvalin or 1x Boreas or 1x Childe\n")
+
+def create_resin_datetime(approxResinTimeLeft):
+        currentResin = 160-approxResinTimeLeft/8
+        for goal in thres:
+                if goal >= currentResin:
+                        requiredResin = goal - currentResin
+                        datetime_for_required_resin = dt.now() + td(minutes=requiredResin*8)
+                        date=datetime_for_required_resin.strftime("%d/%h/%Y")
+                        formatted_12datetime_for_required_resin = datetime_for_required_resin.strftime("%I:%M%p")
+                        ls[12].append("==> "+str(goal)+" resin at "+formatted_12datetime_for_required_resin+", "+date)
 
 if __name__ == '__main__':
+        intro()
         process()
